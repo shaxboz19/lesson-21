0abd1b333988fb63bee176f2ff0765f6947380b3
shaxboz19 <shaxboz.nv@mail.ru>
2024-12-01 20:10:57 +0500

    proekt yaratdik

---
+++ index.js
@@ -0,0 +1,23 @@
+// import dayjs from "dayjs";
+
+// 'KK-OO-YYYY'
+
+// const date = new Date()
+
+// let day = date.getDate()
+// let month = date.getMonth()
+// let year = date.getFullYear()
+
+// if(day < 10) {
+//     day = '0' + day
+// }
+
+// if(month < 10) {
+//     month = '0' + month
+// }
+
+// console.log(`${day}-${month}-${year}`);
+
+console.log("wwww");
+
+// console.log(dayjs().format("YYYY-DD/MM"));
